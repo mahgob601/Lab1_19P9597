dc43a2aa923c4e40bd6d9adbea5ef8e25d004c05
Yassin Mahgoub <mahgob601@gmail.com>
2022-04-22 14:24:50 +0200

    update

--- src/Q2.java
+++ src/Q2.java
@@ -1,45 +1,34 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class Q2
 {
-    public static void main(String[] args) {
-
-        inEquilibrium(4,"-1 0 2","4 5 6", "-99 10 200");
-
-    }
-
-    public static String inEquilibrium(int num, String coord1,String coord2, String coord3) {
-        String[] arrOfStr1 = coord1.split(" ", 3);
-        String[] arrOfStr2 = coord2.split(" ", 3);
-        String[] arrOfStr3 = coord3.split(" ", 3);
-
-        int x, y,z;
-
-        int[] finalArr;
-        finalArr = new int[3];
 
 
-        for( int i= 0; i < arrOfStr1.length; i++)
+    private int iters;
+
+
+    public String inEquilibrium(int iter,int[][] arr)
+    {
+        if(iter < 1 || iter > 100 || iter != arr.length)
         {
-            x = Integer.parseInt(arrOfStr1[i]);
-            y = Integer.parseInt(arrOfStr2[i]);
-            z = Integer.parseInt(arrOfStr3[i]);
+            return("ERROR");
+        }
 
-            finalArr[i] = x + y + z;
-            if(finalArr[i] != 0)
+        int[] result = new int[3];
+        for(int i = 0; i < arr.length; i++)
+        {
+            for(int j = 0; j < 3; j++)
             {
-
-                break;
+                if(arr[i][j] > 100 || arr[i][j] < -100)
+                {
+                    return("ERROR");
+                }
+                result[j] += arr[i][j];
             }
         }
+        if (result[0] == 0 & result[1] == 0 & result[2] == 0)
+            return("YES");
+        else
+            return("NO");
 
-        for (String a : arrOfStr1) {
-            System.out.println(a);
-
-        }
-        return null;
     }
 
 }
---
+++ tests/Q2Test.java
@@ -0,0 +1,11 @@
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Q2Test extends TestCase {
+
+    @Test
+    public void inEquilibrium() {
+    }
+}
