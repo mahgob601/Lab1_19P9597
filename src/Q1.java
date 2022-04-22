e2f7a0d651f759c1c31984c45c1adcba960c9099
Yassin Mahgoub <mahgob601@gmail.com>
2022-04-22 02:57:15 +0200

    update 1 MuTeCa

--- src/Q1.java
+++ src/Q1.java
@@ -2,7 +2,7 @@ public class Q1
 {
     public String fruitDivisor(int weight)
     {
-        if(weight != 2 && weight % 2 == 0)
+        if((weight != 2 && weight != 0) && weight % 2 == 0)
         {
             return("YES");
         }
--- tests/Q1Test.java
+++ tests/Q1Test.java
@@ -1,20 +1,33 @@
 import junit.framework.TestCase;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.Timeout;
 
 import static org.junit.Assert.*;
 
 public class Q1Test {
+    
 
-    @Before
-    public void setUp() throws Exception {
-    }
-
-    @Test
+    @Test(timeout = 1000)
     public void testfruitDivisor()
     {
         Q1 tester = new Q1();
+        Q1 tester2 = new Q1();
         Assert.assertEquals("8 should give YES", "YES", tester.fruitDivisor(8));
+    }
+    @Test(timeout = 1000)
+    public void testfruitDivisorInvalidCase()
+    {
+        Q1 tester = new Q1();
+        Assert.assertEquals("0 should give NO", "NO", tester.fruitDivisor(0));
+    }
+
+    @Test(timeout = 1000)
+    public void testfruitDivisorError()
+    {
+        Q1 tester = new Q1();
+        Assert.assertEquals("7 should give NO", "NO", tester.fruitDivisor(7));
     }
 }
